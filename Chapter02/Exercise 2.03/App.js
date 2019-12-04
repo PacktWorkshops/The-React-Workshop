c7145e9185c1c7170ebeed360c091411614ad01f
RanuKundu2412 <51164503+RanuKundu2412@users.noreply.github.com>
2019-12-04 10:47:27 +0530

    code updated

--- Chapter02/Exercise 2.03/App.js
+++ Chapter02/Exercise 2.03/App.js
@@ -1,9 +1,5 @@
-import React, { Component } from 'react';
-
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
+      this.state = {
       username: '',
       password: '',
       passwordConfirmation: '',
