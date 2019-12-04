34e18963ca0ce3798fc57f65690cf861f7e78e16
RanuKundu2412 <51164503+RanuKundu2412@users.noreply.github.com>
2019-12-04 11:01:34 +0530

    updated

---
+++ Chapter04/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import LifecycleTest from "./LifecycleTest";
+import "./App.css";
+
+class App extends Component {
+  
+  render() {
+    return (
+      <div className="App">
+        Hello Conditional
+        {true && <LifecycleTest />}
+      </div>
+    );
+  }
+}
+
+export default App;
