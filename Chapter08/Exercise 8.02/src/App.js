eb53935e9f405a90d6bf09312c1c070b6efbac1c
RanuKundu2412 <51164503+RanuKundu2412@users.noreply.github.com>
2020-04-06 14:57:50 +0530

    updated

--- Chapter08/Exercise 8.02/src/App.js
+++ Chapter08/Exercise 8.02/src/App.js
@@ -1,33 +1,13 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-} from 'react-router-dom';
 import {ControlledForm} from './ControlledForm';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <div>
-          <ul>
-            <li>
-              <Link to="/">Controlled Form Component</Link>
-            </li>
-          </ul>
-          <hr />
-          <Switch>
-            <Route exact path="/">
-              <ControlledForm />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
-    </div>
+  <div className="App”>
+  <ControlledForm />
+  </div>
   );
 }
-
 export default App;
+
