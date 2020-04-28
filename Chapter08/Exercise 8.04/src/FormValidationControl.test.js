06ed837051b12e76030f22e4262187f499ff2292
Endre Vegh <endre.vegh@yahoo.com>
2020-04-28 08:34:38 +0200

    cha4

--- Chapter08/Exercise 8.04/src/FormValidationControl.test.js
+++ Chapter08/Exercise 8.04/src/FormValidationControl.test.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import {render, fireEvent, wait} from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
+import React from "react";
+import { render, fireEvent, wait } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
 
-import FormValidationControl from './FormValidationControl';
+import FormValidationControl from "./FormValidationControl";
+import { act } from "react-dom/test-utils";
 
-test('renders correctly', () => {
-  const {container} = render(<FormValidationControl/>);
+test("renders correctly", () => {
+  const { container } = render(<FormValidationControl />);
+
   expect(container).toMatchSnapshot();
 });
 
-test('loads and displays form', async () => {
+test("loads and displays form", async () => {
   //Arrange
   const { container } = render(<FormValidationControl />);
   const email = container.querySelector('input[type="text"]');
@@ -19,24 +21,25 @@ test('loads and displays form', async () => {
   await wait(() => {
     fireEvent.change(email, {
       target: {
-        value: 'example@email.com'
-      }
+        value: "example@email.com",
+      },
     });
+
     fireEvent.change(password, {
       target: {
-        value: 'password'
-      }
-    })
+        value: "password",
+      },
+    });
   });
 
   //Assert
-  expect(email.value).toBe('example@email.com');
-  expect(password.value).toBe('password');
+  expect(email.value).toBe("example@email.com");
+  expect(password.value).toBe("password");
 });
 
-test('displays an error message', async () => {
+test("displays an error message", async () => {
   //Arrange
-  const { container } = render(<FormValidationControl />);
+  const { container, queryByText } = render(<FormValidationControl />);
   const email = container.querySelector('input[type="text"]');
   const password = container.querySelector('input[type="password"]');
 
@@ -44,18 +47,86 @@ test('displays an error message', async () => {
   await wait(() => {
     fireEvent.change(email, {
       target: {
-        value: ''
-      }
+        value: "",
+      },
     });
     fireEvent.change(password, {
       target: {
-        value: ''
-      }
+        value: "",
+      },
     });
     fireEvent.blur(email);
     fireEvent.blur(password);
   });
 
-  //Assert
-  expect(container).toMatchSnapshot();
+  const emailErrorMessage = queryByText(/Name is Required/);
+  const passwordErrorMessage = queryByText(/Min length of Password is 8 chars/);
+
+  expect(emailErrorMessage).not.toBeNull();
+  expect(passwordErrorMessage).not.toBeNull();
+});
+
+test("displays an error message", async () => {
+  // mock info, used in submission, so we can spy on it
+  let originalInfo = console.info;
+
+  global.console = { ...global.console, info: jest.fn() };
+
+  // mock setTimeout in submission, so we can advance it
+  jest.useFakeTimers();
+
+  //Arrange
+  const { container, queryByText } = render(<FormValidationControl />);
+
+  const email = container.querySelector('input[type="text"]');
+  const password = container.querySelector('input[type="password"]');
+  const form = container.querySelector("form");
+
+  // values that passes validation
+  const emailValue = "hey@ho.com";
+  const passwordValue = "passwordpass";
+
+  //Act
+  await wait(() => {
+    fireEvent.change(email, {
+      target: {
+        value: emailValue,
+      },
+    });
+    fireEvent.change(password, {
+      target: {
+        value: passwordValue,
+      },
+    });
+    fireEvent.blur(email);
+    fireEvent.blur(password);
+  });
+
+  let emailErrorMessage = queryByText(/Name is Required/);
+  let passwordErrorMessage = queryByText(/Min length of Password is 8 chars/);
+
+  // error messages should not be there if the fields are passed validation
+  expect(emailErrorMessage).toBeNull();
+  expect(passwordErrorMessage).toBeNull();
+
+  // submit form
+  await wait(() => {
+    fireEvent.submit(form);
+  });
+
+  // advance setTimeout
+  act(() => {
+    jest.advanceTimersByTime(3000);
+  });
+
+  const expectedLog = JSON.stringify(
+    { name: emailValue, password: passwordValue },
+    null,
+    2
+  );
+
+  // assert against console.info
+  expect(console.info).toHaveBeenCalledWith(expectedLog);
+
+  global.console = { ...global.console, info: originalInfo };
 });
--- Chapter08/Exercise 8.04/src/FormYupValidation.test.js
+++ Chapter08/Exercise 8.04/src/FormYupValidation.test.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import {render, fireEvent, wait} from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
+import React from "react";
+import { render, fireEvent, wait } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
 
-import FormYupValidation from './FormYupValidation';
+import FormYupValidation from "./FormYupValidation";
+import { act } from "react-dom/test-utils";
 
-test('renders correctly', () => {
-  const {container} = render(<FormYupValidation/>);
+test("renders correctly", () => {
+  const { container } = render(<FormYupValidation />);
+
   expect(container).toMatchSnapshot();
 });
 
-test('loads and displays form', async () => {
+test("loads and displays form", async () => {
   //Arrange
   const { container } = render(<FormYupValidation />);
   const email = container.querySelector('input[type="text"]');
@@ -19,25 +21,25 @@ test('loads and displays form', async () => {
   await wait(() => {
     fireEvent.change(email, {
       target: {
-        value: 'example@email.com'
-      }
+        value: "example@email.com",
+      },
     });
+
     fireEvent.change(password, {
       target: {
-        value: 'password'
-      }
-    })
+        value: "password",
+      },
+    });
   });
 
   //Assert
-  expect(email.value).toBe('example@email.com');
-  expect(password.value).toBe('password');
+  expect(email.value).toBe("example@email.com");
+  expect(password.value).toBe("password");
 });
 
-
-test('displays an error message', async () => {
+test("displays an error message", async () => {
   //Arrange
-  const { container } = render(<FormYupValidation />);
+  const { container, queryByText, getByText } = render(<FormYupValidation />);
   const email = container.querySelector('input[type="text"]');
   const password = container.querySelector('input[type="password"]');
 
@@ -45,18 +47,77 @@ test('displays an error message', async () => {
   await wait(() => {
     fireEvent.change(email, {
       target: {
-        value: ''
-      }
+        value: "",
+      },
     });
     fireEvent.change(password, {
       target: {
-        value: ''
-      }
+        value: "",
+      },
     });
     fireEvent.blur(email);
     fireEvent.blur(password);
   });
 
-  //Assert
-  expect(container).toMatchSnapshot();
+  const emailErrorMessage = getByText(/Required/);
+  const passwordErrorMessage = queryByText(/Min length of Password is 8 chars/);
+
+  expect(emailErrorMessage).not.toBeNull();
+  expect(passwordErrorMessage).not.toBeNull();
+});
+
+test("displays an error message", async () => {
+  let originalInfo = console.info;
+
+  global.console = { ...global.console, info: jest.fn() };
+  jest.useFakeTimers();
+
+  //Arrange
+  const { container, queryByText } = render(<FormYupValidation />);
+  const email = container.querySelector('input[type="text"]');
+  const password = container.querySelector('input[type="password"]');
+  const form = container.querySelector("form");
+
+  const emailValue = "hey@ho.com";
+  const passwordValue = "passwordpass";
+
+  //Act
+  await wait(() => {
+    fireEvent.change(email, {
+      target: {
+        value: emailValue,
+      },
+    });
+    fireEvent.change(password, {
+      target: {
+        value: passwordValue,
+      },
+    });
+    fireEvent.blur(email);
+    fireEvent.blur(password);
+  });
+
+  let emailErrorMessage = queryByText(/Required/);
+  let passwordErrorMessage = queryByText(/Min length of Password is 8 chars/);
+
+  expect(emailErrorMessage).toBeNull();
+  expect(passwordErrorMessage).toBeNull();
+
+  await wait(() => {
+    fireEvent.submit(form);
+  });
+
+  act(() => {
+    jest.advanceTimersByTime(3000);
+  });
+
+  const expectedLog = JSON.stringify(
+    { name: emailValue, password: passwordValue },
+    null,
+    2
+  );
+
+  expect(console.info).toHaveBeenCalledWith(expectedLog);
+
+  global.console = { ...global.console, info: originalInfo };
 });
