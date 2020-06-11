89cee224ee8c62a2208a02abb3086432aaa6de0d
Cihan Yakar <yakar@omegabigdata.com>
2020-06-11 19:20:17 +0300

    Modified according to the exercise c4.e1.

--- Chapter04/Exercise 4.01/src/App.js
+++ Chapter04/Exercise 4.01/src/App.js
@@ -1,28 +1,30 @@
 import React, { Component } from 'react';
-
+import "./App.css";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    console.log('Constructor');
+    console.log("Constructor");
     this.state = { cycle: 0 };
-    setInterval(() => this.setState({ cycle: this.state.cycle + 1 }), 1000);
-  }
+    setInterval(
+      () => this.setState({ cycle: this.state.cycle + 1 }),
+      1000
+    );
+  } 
 
-  shouldComponentUpdate(nextProps, nextState)
-  {
-  console.log("Should Component Update");
-  return this.props !== nextProps || this.state !== nextState;
-  }
-
+  componentDidMount() {
+    console.log("Component Did Mount");
+    }
+    
   componentDidUpdate() {
-    console.log('Component Did Update');
-   
+    console.log("Component Did Update");
   }
 
   render() {
-    console.log('Render');
-    return <div className="App">Hello Lifecycle: Cycle {this.state.cycle}</div>;
+    console.log("Render");
+    return (
+        <div className="App">Hello Lifecycle: Cycle {this.state.cycle}</div>
+    );
   }
 }
 
