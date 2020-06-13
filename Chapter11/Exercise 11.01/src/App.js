802c6d54855f1b3c5d9fe668403c85d86cb79293
Cihan Yakar <yakar@omegabigdata.com>
2020-06-13 12:54:46 +0100

    Modified according to the exercise c11.e1.

--- Chapter11/Exercise 11.01/src/App.js
+++ Chapter11/Exercise 11.01/src/App.js
@@ -1,22 +1,16 @@
 import React from "react";
 import "./App.css";
 
-const useToggle = initialValue => {
-  const [value, setValue] = React.useState(initialValue);
-  const toggle = () => setValue(prevValue => !prevValue);
-  return [value, toggle];
-};
-
 const App = () => {
   const url = "https://images.unsplash.com/photo-1562051036-e0eea191d42f";
-  const [displayImage, toggleState] = useToggle(false);
-  const toggleImage = () => toggleState(!displayImage);
+  const [displayImage, setState] = React.useState(false);
+  const toggleImage = () => setState(!displayImage);
   return (
-    <div className="App">
-      {displayImage && <img src={url} alt="Some coffee beans" />}
-      <br />
-      <button onClick={toggleImage}>Toggle Image Display</button>
-    </div>
+  <div className="App">
+    {displayImage && <img src={url} alt="Some coffee beans" />}
+    <br />
+    <button onClick={toggleImage}>Toggle Image Display</button>
+  </div>
   );
 };
 
